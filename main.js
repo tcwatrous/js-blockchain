2971f7ad444d465c70a2e0e28f9fda3952711616
tim <tim@omise.co>
2019-02-12 16:42:37 +0700

    chain object

--- main.js
+++ main.js
@@ -11,10 +11,45 @@ class Block {
   }
 
   calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString()
   }
 
   mineBlock(difficulty) {
 
+  }
+}
+class BlockChain {
+  constructor(){
+    this.chain = [this.createGenesis()];
+  }
+
+  createGenesis(){
+    return new Block(0, "02/12/2019", "Genesis block", "0")
+  }
+
+  latestBlock(){
+    return this.chain[this.chain.length-1]
+  }
+
+  addBlock(newBlock){
+      newBlock.previousHash = this.latestBlock().hash;
+      newBlock.hash = newBlock.calculateHash();
+      this.chain.push(newBlock);
+  }
+
+  checkValid(){
+    for(let i = 1; i < this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i-1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash){
+        return false;
+      }
+      if(currentBlock.previousHash !== previousBlock.hash){
+        return false;
+      }
+    }
+
+    return true;
   }
 }
