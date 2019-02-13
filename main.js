b1ec3e5d0266e7310c822100aecf3bc6654de4ae
tim <tim@omise.co>
2019-02-13 11:00:20 +0700

    wallet class

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const SHA256 = require('crypto-js/sha256')
 
 class Block {
-  constructor(timestamp, data){
-    this.index = 0;
+  constructor(index, timestamp, data){
+    this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = "0";
     this.hash = this.calculateHash();
-    this.nonce = 0;
+    this.nonce = this.generateNonce();
   }
 
   calculateHash(){
@@ -15,12 +15,37 @@ class Block {
   }
 
   mineBlock(difficulty) {
+    let difficultyStr = "";
+    for(let i = 0; i < difficulty ; i++){
+      difficultyStr+="0"
+    }
+    console.log(difficultyStr);
+    while(!(this.hash.substring(0,difficulty) === difficultyStr)){
+      this.nonce = this.generateNonce();
+      this.hash = this.calculateHash();
+      console.log("nonce: " + this.nonce);
+      console.log("hash: " + this.hash);
+    }
+    console.log("final nonce: " + this.nonce);
+    console.log("final hash: " + this.hash);
+    console.log("final hash calc: " + this.calculateHash());
+  }
 
+  generateNonce() {
+    var text = "";
+    var possible = "0123456789abcedf";
+    for(let i = 0; i < 32; i++) {
+      text += possible.charAt(Math.floor(Math.random()*possible.length));
+    }
+    return text;
   }
 }
-class BlockChain {
+
+
+class Blockchain {
   constructor(){
     this.chain = [this.createGenesis()];
+    this.difficulty = 4;
   }
 
   createGenesis(){
@@ -33,7 +58,7 @@ class BlockChain {
 
   addBlock(newBlock){
       newBlock.previousHash = this.latestBlock().hash;
-      newBlock.hash = newBlock.calculateHash();
+      newBlock.mineBlock(this.difficulty);
       this.chain.push(newBlock);
   }
 
@@ -42,7 +67,7 @@ class BlockChain {
       const currentBlock = this.chain[i];
       const previousBlock = this.chain[i-1];
 
-      if(currentBlock.hash !== currentBlock.calculateHash){
+      if(currentBlock.hash !== currentBlock.calculateHash()){
         return false;
       }
       if(currentBlock.previousHash !== previousBlock.hash){
@@ -53,3 +78,27 @@ class BlockChain {
     return true;
   }
 }
+
+class Wallet {
+  constructor() {
+    this.privateKey = this.generatePrivateKey();
+    this.publicKey = SHA256(this.privateKey);
+    this.amount = 0;
+  }
+
+  generatePrivateKey(){
+    var text = "";
+    var possible = "0123456789abcedf";
+    for(let i = 0; i < 64; i++) {
+      text += possible.charAt(Math.floor(Math.random()*possible.length));
+    }
+    return text;
+  }
+}
+
+let jsChain = new Blockchain();
+jsChain.addBlock(new Block(1, "02/13/2019", {amount: 5}));
+jsChain.addBlock(new Block(2, "02/14/2019", {amount: 10}));
+
+console.log(JSON.stringify(jsChain, null, 4));
+console.log("Is blockchain valid? " + jsChain.checkValid());
